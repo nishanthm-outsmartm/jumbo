e97001d1d6d66bb410e5dc177d349391efee38d8
nishanthh19 <nishanthm@outsmartm.co.uk>
2025-08-23 01:11:46 +0530

    profile changess

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -14,12 +14,12 @@ const config = {
 
 
   firebase: {
-        API_KEY: "AIzaSyAo0hS9IHLUP9gXbZYq8GtlUUXGwOJWrKk",
-        APP_ID: "1:797827104422:web:e6b002e3d2f7511757fe67",
-        PROJECT_ID: "sample-9b8aa",
-        privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDeGgHyWxD5TxFW\nmY2VPYT9Pg3pzjLpgzMaAYANENEbw2HZ8ugdbHvgAsyiAZJkg3xygeQfb50KJ4k3\n49lHny282Z/1lSHVLbdXto6n2s/1fiSKwnWataCCaK7FG4rMlKN/iFvvLYTMm6IA\n5/nXEbwelkcSDljIpwkvyb1LBpPXXCbVMfoF90vv3MYuYKWVQAk0uInpY++Dewe+\njqIQBD8Hp+YHYmcYyjE0OW6RlzKIERUv7VwiSrwcu0IjHEzMXFH7TUr98LV+F/99\n0Fts2zMPK96ZnJdadJlCR0l3vh6Ggm7ibYSp2xyihQ+IG4fFBeSzyX31304gwViX\nB2yE/ts5AgMBAAECggEAO+LZWYbSidaW/znCIlaHMF12FO/jbHFS6xUyeNNq/shs\n7v5/NcfVku5OTPSNi4qkmJ+LaiqHNoC4kBhgst4pz/0CE47Vo4yDHiR8TKhEYJZV\nthB+82s7z034qe8mOsltWGrlFxW39A+W3BWwmRwQFFNC8Ixrxha79GW6I6uvW2bd\n7TNvQLa4O3K3pO7b51tdl9LLD5YI9xEeGg9yP+K1ramhFl42CbrHLhCnBB0HLOFk\nekzXhSpDhVNKAd2Incyq8eF9E10yvjAOENZyIvFGsm/60dxsAlciIMSV3uzJar6o\nUX2KdIDp7Dp3gnPyAd4pHUFq4n2bdQ4nm8x0NEiC/QKBgQD2VhDfP+NlE5dd9XV3\naH0tNiwo01udmVZDDSp99WVBxlElhxfvCVRypWjpZ7Q+aUEYgZMP4YJ5Drek1jex\nljpPpheiIg4zeDftddyeqIZDRiOyI/GtupDATzOLr8ha0CNBWo14zB+jGBnHgcGz\nmoCylJt+pH3at4oTiQiNChHoEwKBgQDm0I46iqcEqGTT/wzdczjbVvT92+jCYDYq\nL7XBoYZS/5taAWx+O5NXt4LPh2KUenpgynA72ok7xhMJzkwyKUsRqqmHAUHf1dMk\n8+FTIvDDog1Ty0+0SW3yA3ObbMxXV4J/DArQE91tGCPOZJx9yGIB0OnC7/Z+dHpZ\nd7Eit9yxAwKBgQCWhbS9lh5vLu95XHmww0pqDAYj1DLRNFc/lZX3bbG35WTEV65K\nRBs0Ctr2dIXw9QPv441nUNcW/eU9diCGBVQQQ1+BlOm0jasvEKwEztlr1joNqmUN\nUUkOGddXbcwxoalJT/nObHyQfjOSczlXc/tcI+54Di9RLIHUuhFdjV+vvQKBgQCj\nL+qO6z6gZWo8FEQFQZR+RyGZZeRPoPRKqUao6slsXvl4Di+lo/tNdhpPHZoN/fYN\n/rKRLrX+A9GsCiYHBPjlObL4F2R6m9PXeXHidH6i4izoBIOKpKvOArC2v5pNWjqS\nFnKCoLjZi8qs/qdBqu3X+Nt9DmlJCJMENEKRmEq4/wKBgCGnF+s+o2J0yPw+7JCw\nbsiUzsGWEzHlu9ghBiJ7TNT9LHM4/pE/LN7uVJ9FiQX8kvyq3/E7XGzSFb3xe9dS\nyb4iB5KSyx7iImQA5/JEbVTgyTjxXptlGjjvPLiEgj+c6amh1gN2DFwmtLnJDWNc\nWk/3YAzWUNmT2A2i/Fj860no\n-----END PRIVATE KEY-----\n",
-        clientEmail: "firebase-adminsdk-fbsvc@sample-9b8aa.iam.gserviceaccount.com",
-     },
+        API_KEY: "",
+        APP_ID: "",
+        PROJECT_ID: "-",
+        privateKey: "-----BEGIN PRIVATE KEY-----",
+        clientEmail: "",
+     }, 
   },
   loadingScreenTimeout: 100,
 };
